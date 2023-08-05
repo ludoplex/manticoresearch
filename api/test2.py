6a17d228731dade1dc8873af2760745662a35a02
Sourcery AI <>
2023-08-05 09:43:13 +0000

    'Refactored by Sourcery'

--- api/test2.py
+++ api/test2.py
@@ -12,15 +12,12 @@ index = 'forum'
 opts = {'before_match':'<b>', 'after_match':'</b>', 'chunk_separator':' ... ', 'limit':400, 'around':15}
 
 cl = SphinxClient()
-res = cl.BuildExcerpts(docs, index, words, opts)
-
-if not res:
-	print  ('ERROR: %s\n' % cl.GetLastError())
-else:
-	n = 0
-	for entry in res:
-		n += 1
+if res := cl.BuildExcerpts(docs, index, words, opts):
+	for n, entry in enumerate(res, start=1):
 		print ('n=%d, res=%s' % (n, entry))
+
+else:
+	print  ('ERROR: %s\n' % cl.GetLastError())
 
 #
 # $Id$
